761bfa770cfb55ee470dd489d32961f59e8bc6ea
田玉星 <tianyuxing91@sina.com>
2022-09-22 05:42:22 +0800

    fix

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @author Tian.Yuxing
+ * @email tianyuxing@sciall.org.cn
+ * Date: 2022/9/22
+ * Time: 05:41
+ */
+phpinfo();
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tyx/composer_frame',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8e92d79c1ee1162c3558f773b497719ad7c8baa7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tyx/composer_frame' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8e92d79c1ee1162c3558f773b497719ad7c8baa7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
